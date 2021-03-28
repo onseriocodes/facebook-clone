9ad4d0a34eee0f308ef325e84aaae8fbf885a88a
tony onserio <onseriocodes@gmail.com>
2021-03-28 12:09:58 -0500

    INITIAL COMMIT

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='app'>
+      <h1>FACEBOOK CLONE</h1>
+    </div>
+  );
+};
+
+export default App;
+
+// npm install -g firebase-tools
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyAPYmCMU-NoUk81G4ecLFaSUcBiKV1KdG4',
+  authDomain: 'facebook-clone-673d9.firebaseapp.com',
+  projectId: 'facebook-clone-673d9',
+  storageBucket: 'facebook-clone-673d9.appspot.com',
+  messagingSenderId: '350295664527',
+  appId: '1:350295664527:web:044915c3e100ab2e0c4dfa',
+  measurementId: 'G-S8FD544VFE'
+};
